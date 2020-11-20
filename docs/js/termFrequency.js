82a27310fd12e324b579de4ecead2e6262e1a995
Gabriel Aquino <gabrielsaquino@tecgraf.puc-rio.br>
2020-11-20 12:56:53 -0300

    Debugando erro no carregamento do arquivo

--- docs/js/termFrequency.js
+++ docs/js/termFrequency.js
@@ -61,6 +61,7 @@ function loadFileAsText(){
     var fileReader = new FileReader();
     fileReader.onload = function(fileLoadedEvent){
         var textFromFileLoaded = fileLoadedEvent.target.result;
+        console.log(textFromFileLoaded)
         $("#fileTextArea").val(textFromFileLoaded);
         textLoaded = true;
     }
@@ -75,6 +76,7 @@ function loadStopWordsAsText(){
     fileReader.onload = function(fileLoadedEvent){
         var textFromFileLoaded = fileLoadedEvent.target.result;
         //document.getElementById("inputTextToSave").innerHTML = textFromFileLoaded;
+        console.log(textFromFileLoaded)
         $("#stopWordsTextArea").val(textFromFileLoaded);
         stopWordLoaded = true
     }
